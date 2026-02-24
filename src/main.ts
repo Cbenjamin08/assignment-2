bca8958e084f7c03a472e45888bc7ddd03f95249
unknown <343137386@TDSB.ON.CA>
2026-02-24 10:15:51 -0500

    added form and basic css

---
+++ src/main.ts
@@ -0,0 +1,22 @@
+const form = document.getElementById("cubic-form") as HTMLFormElement;
+
+form?.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const formData = new FormData(form);
+
+    const a: number = Number(formData.get("a"));
+    const b: number = Number(formData.get("b"));
+    const c: number = Number(formData.get("c"));
+    const d: number = Number(formData.get("d"));
+
+    const discriminant = a + b + c + d;
+
+    if (discriminant < 0) {
+        (document.getElementById("result") as HTMLInputElement).value = "No Roots";
+    } else if (discriminant > 0) {
+        (document.getElementById("result") as HTMLInputElement).value = `x=${discriminant}`;
+    } else {
+        (document.getElementById("result") as HTMLInputElement).value = `x=${discriminant}`;
+    }
+})
